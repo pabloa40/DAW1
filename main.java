f8e1f428e7a96f268ba2814e56f071ec2eb31aaf
Pablo Rubio Amondarain <pablorubioamodnarain@gmail.com>
2021-09-14 10:15:35 +0200

    ahora funciona

---
+++ main.java
@@ -0,0 +1,8 @@
+import a.java;
+
+public static void main(String[] args){
+    
+    public a = new a();
+
+    System.out.println("esto funciona más oi menos");
+}
---
+++ prueba1/Adfas.java
@@ -0,0 +1,16 @@
+package prueba1;
+
+
+public class Adfas{
+
+    public static void main(String[] args)
+    {
+        a aaaa= new a();
+
+        aaaa.sampleMethod(4);
+
+        System.out.println("esto va");
+    }
+    
+
+}
